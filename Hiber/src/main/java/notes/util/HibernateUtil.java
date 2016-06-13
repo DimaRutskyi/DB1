390a8307d4c713778ebdce4b40a5642b556f4db5
dima_rutskiy <drutskey@gmail.com>
2016-06-13 18:47:17 +0300

    add my web module

--- Hiber/src/main/java/notes/util/HibernateUtil.java
+++
@@ -1,7 +0,0 @@
-package notes.util;
-
-/**
- * Created by Rrr on 09.02.2016.
- */
-public class HibernateUtil {
-}
--- dima_rutskiy/src/main/java/MongoDB/MongoDB.java
+++
@@ -1,7 +0,0 @@
-package MongoDB;
-
-/**
- * Created by Rrr on 09.06.2016.
- */
-public class MongoDB {
-}
